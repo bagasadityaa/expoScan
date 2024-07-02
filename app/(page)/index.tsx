1e6b71bd8a2129af1f5c676961fc3a60c8fe9890
bagas aditya <bagasaditya599@gmail.com>
2024-07-02 23:45:04 +0700

    commit

--- app/(page)/index.tsx
+++ app/(page)/index.tsx
@@ -1,24 +1,56 @@
-import { StyleSheet, Text, View } from "react-native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { CameraView, Camera } from "expo-camera";
+import { Link, useNavigation } from "expo-router";
+import React, { useState, useEffect } from "react";
+import { Text, View, StyleSheet, Button } from "react-native";
+// import { CameraView, Camera } from "expo-camera/next";
 
-export default function app() {
+export default function App({}) {
+  const navigation = useNavigation();
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+
+  useEffect(() => {
+    const getCameraPermissions = async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === "granted");
+    };
+
+    getCameraPermissions();
+  }, []);
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true);
+    alert(`Code anda adalah ${data}`);
+    navigation.navigate("settings");
+  };
+
+  if (hasPermission === null) {
+    return <Text>Requesting for camera permission</Text>;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
   return (
     <View style={styles.container}>
-      <View style={styles.containerHai}>
-        <Text>Haii ini app</Text>
-      </View>
+      <CameraView
+        onBarcodeScanned={scanned ? undefined : handleBarCodeScanned}
+        barcodeScannerSettings={{
+          barcodeTypes: ["qr", "pdf417"],
+        }}
+        style={StyleSheet.absoluteFillObject}
+      />
+      {scanned && (
+        <Button title={"Tap to Scan Again"} onPress={() => setScanned(false)} />
+      )}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    width: "100%",
-    height: "100%",
-  },
-  containerHai: {
-    marginTop: "9%",
-    display: "flex",
-    alignItems: "center",
-    width: "100%",
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "center",
   },
 });
