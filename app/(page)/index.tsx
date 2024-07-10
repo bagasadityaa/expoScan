adae5d40489c090ebdf0c09f91378fd099285b46
bagas aditya <bagasaditya599@gmail.com>
2024-07-10 10:43:11 +0700

    commit

--- app/(page)/index.tsx
+++ app/(page)/index.tsx
@@ -1,19 +1,40 @@
-import { Text, View } from "react-native";
+import Card from "@/components/Card";
+import * as React from "react";
+import { Text, View, useWindowDimensions } from "react-native";
+import { TabView, SceneMap } from "react-native-tab-view";
 
-import { useSession } from "../auth/ctx";
-
-export default function Index() {
-  const { signOut } = useSession();
+const FirstRoute = () => {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text
-        onPress={() => {
-          // The `app/(app)/_layout.tsx` will redirect to the sign-in screen.
-          signOut();
-        }}
-      >
-        Sign Out
-      </Text>
+    <View style={{ flex: 1 }}>
+      <Card />
     </View>
+  );
+};
+const SecondRoute = () => (
+  <View style={{ flex: 1, backgroundColor: "#673ab7" }} />
+);
+
+const renderScene = SceneMap({
+  first: FirstRoute,
+  second: SecondRoute,
+});
+
+export default function TabViewExample() {
+  const layout = useWindowDimensions();
+
+  const [index, setIndex] = React.useState(0);
+  const [routes] = React.useState([
+    { key: "first", title: "First" },
+    { key: "second", title: "Second" },
+  ]);
+
+  return (
+    <TabView
+      navigationState={{ index, routes }}
+      renderScene={renderScene}
+      onIndexChange={setIndex}
+      initialLayout={{ width: layout.width }}
+      style={{ marginTop: "10%" }}
+    />
   );
 }
